4b9822f87b135163be6cb0ae9f3350c2831ce943
ezgi <ezgiozdikyar@gmail.com>
2023-04-18 22:30:20 +0300

    Add project files.

---
+++ DelayedJobs/Business/MailManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+namespace DelayedJobs.Business
+{
+    public class MailManager
+    {
+
+        private readonly IConfiguration _configuration;
+        public MailManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SendMail(string email)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+
+            using (var client = new SmtpClient(emailSettings["Host"], int.Parse(emailSettings["Port"])))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(emailSettings["UserName"], emailSettings["Password"]);
+                client.EnableSsl = true;
+
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress(emailSettings["UserName"]);
+                    message.To.Add(email);
+                    message.Subject = "Confirm your account";
+                    message.Body = "Please click the link below to confirm your account.";
+
+                    client.SendMailAsync(message);
+                }
+            }
+        }
+    }
+}
---
+++ DelayedJobs/Controllers/MailsController.cs
@@ -0,0 +1,18 @@
+﻿using DelayedJobs.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelayedJobs.Controllers
+{
+    [Route("api/mail")]
+    [ApiController]
+    public class MailsController : ControllerBase
+    {
+        [HttpPost]
+        public void SendEmail(string email)
+        {
+            Hangfire.BackgroundJob.Schedule<MailManager>(
+               j => j.SendMail(email),
+               TimeSpan.FromSeconds(30));
+        }
+    }
+}
